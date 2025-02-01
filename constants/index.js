f23e51cbd1792f1a6a601d2d79383cc5b8637ea5
DigitalHerencia <DigitalHerencia@outlook.com>
2025-01-31 19:11:21 -0700

    fixed constants

--- constants/index.js
+++ constants/index.js
@@ -1,30 +1,30 @@
 export const sidebarLinks = [
   {
-    id: "home",
     imgURL: "/assets/home.svg",
     route: "/",
     label: "Home",
   },
   {
-    id: "search",
     imgURL: "/assets/search.svg",
     route: "/search",
     label: "Search",
   },
   {
-    id: "activity",
     imgURL: "/assets/heart.svg",
     route: "/activity",
     label: "Activity",
   },
   {
-    id: "create-thread",
     imgURL: "/assets/create.svg",
     route: "/create-thread",
-    label: "Create Drop",
+    label: "Create Thread",
   },
   {
-    id: "profile",
+    imgURL: "/assets/community.svg",
+    route: "/communities",
+    label: "Communities",
+  },
+  {
     imgURL: "/assets/user.svg",
     route: "/profile",
     label: "Profile",
@@ -35,4 +35,10 @@ export const profileTabs = [
   { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
   { value: "replies", label: "Replies", icon: "/assets/members.svg" },
   { value: "tagged", label: "Tagged", icon: "/assets/tag.svg" },
+];
+
+export const communityTabs = [
+  { value: "threads", label: "Threads", icon: "/assets/reply.svg" },
+  { value: "members", label: "Members", icon: "/assets/members.svg" },
+  { value: "requests", label: "Requests", icon: "/assets/request.svg" },
 ];
