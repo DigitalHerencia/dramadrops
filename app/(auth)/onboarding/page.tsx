85e9f80f586dba1243c7f2814520c7be93a46d06
DigitalHerencia <DigitalHerencia@outlook.com>
2025-01-31 22:05:55 -0700

    onboarding

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -24,11 +24,11 @@ async function Page() {
   return (
     <main className='mx-auto flex max-w-3xl flex-col justify-start px-10 py-20'>
       <h1 className='head-text'>Onboarding</h1>
-      <p className='text-base-regular text-light-2 mt-3'>
+      <p className='mt-3 text-base-regular text-light-2'>
         Complete your profile to use Drama Drops.
       </p>
 
-      <section className='bg-dark-2 mt-9 p-10'>
+      <section className='mt-9 bg-dark-2 p-10'>
         <AccountProfile user={userData} btnTitle='Continue' />
       </section>
     </main>
--- lib/validations/user.ts
+++ lib/validations/user.ts
@@ -2,6 +2,10 @@ import * as z from "zod";
 
 export const UserValidation = z.object({
   profile_photo: z.string().url().nonempty(),
+  name: z
+    .string()
+    .min(3, { message: "Minimum 3 characters." })
+    .max(30, { message: "Maximum 30 caracters." }),
   username: z
     .string()
     .min(3, { message: "Minimum 3 characters." })
